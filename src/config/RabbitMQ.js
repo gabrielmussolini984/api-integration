f228630d82bfb1c8e0c8e8b81af84b48f1b869f2
Gabriel <gabrielmussolini@hotmail.com>
2020-12-30 14:57:36 -0300

    Add Rabbitmq

---
+++ src/config/RabbitMQ.js
@@ -0,0 +1,23 @@
+import { connect } from 'amqplib';
+
+export default class RabbitmqServer {
+  constructor(uri) {
+    this.uri = uri;
+  }
+
+  async start() {
+    this.conn = await connect(this.uri);
+    this.channel = await this.conn.createChannel();
+  }
+
+  async publishInExchange(exchange, routingKey, message) {
+    return this.channel.publish(exchange, routingKey, Buffer.from(message));
+  }
+
+  async consume(queue, callback) {
+    return this.channel.consume(queue, (message) => {
+      callback(message);
+      this.channel.ack(message);
+    });
+  }
+}
